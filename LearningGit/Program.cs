f7eb2f9c7b7d26eb9409ac92e30b8dbd97901917
unknown <cpuneeth@cpuneeth-PC.ssdi.sharp.co.in>
2016-05-20 15:38:45 +0530

    Welcome to Git10

--- LearningGit/Program.cs
+++ LearningGit/Program.cs
@@ -32,6 +32,16 @@ namespace simple
             Console.WriteLine("WELCOME TO  GIT5");
             Console.WriteLine("WELCOME TO  GIT4");
             Console.WriteLine("WELCOME TO  GIT5");
+
+            Console.WriteLine("hi this ism sample");
+            Console.WriteLine("hi this ism sample1");
+            Console.WriteLine("hi this ism sample2");
+            Console.WriteLine("hi this ism sample3");
+
+            Console.WriteLine("hi this ism sample10");
+            Console.WriteLine("hi this ism sample11");
+            Console.WriteLine("hi this ism sample12");
+            Console.WriteLine("hi this ism sample13");
         }
 
     }
